0eeb611f515fdcaa240cc57eead5a3c3ed6610d1
Simba <idrlzt@outlook.com>
2020-12-14 15:32:16 +0800

    fix: preview path error

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 
 import simple from './src/simple';
 import pro from './src/pro';
-import preview from './preview';
+import preview from './src/preview';
 
 export default simple;
 export const MarkdownPro = pro ;
